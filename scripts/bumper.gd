684ab3774470f99388e3641269f74e6a5af71a7e
phloxpha <rambo8000@gmail.com>
2026-01-31 23:41:27 +0100

    Bumper!
    Base de scène et script pour les bumpers.

---
+++ scripts/bumper.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var force: float =  25.0
+var force_direction: Vector2 
+
+var debug_bumper_position : Vector2 = Vector2.ZERO
+var debug_force_direction : Vector2 = Vector2.ZERO
+
+func _on_body_entered(body: Node2D) -> void:
+	force_direction = body.get_position() - get_position()
+	
+	if body.is_in_group("player"):
+		body.apply_central_impulse(Vector2(force_direction * force))
+		
+	$AudioStreamPlayer.pitch_scale = randf_range(0.8,1.2)
+	$AudioStreamPlayer.play()
